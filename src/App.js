be4fcc81ed63c46442c51d00138b51e6f0b725db
sabrinapalmer <sabrinampalmer@gmail.com>
2023-06-18 11:52:48 -0500

    fixed router issue

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/spotify-playlist-app" element={<Home />} />
         <Route path="/callback" element={<Callback />} />
       </Routes>
     </Router>
