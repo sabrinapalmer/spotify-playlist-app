2167dd7bb20e29fe23ce7c32656cc52f6b823cc4
sabrinapalmer <sabrinampalmer@gmail.com>
2023-06-20 18:44:44 -0500

    back to browser router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import Callback from './components/Callback';
