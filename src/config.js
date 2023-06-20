0473b203df44b3821988d6271ec23fbe2abcc541
sabrinapalmer <sabrinampalmer@gmail.com>
2023-06-20 12:25:03 -0500

    fixed callback url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,4 @@
 const clientId = '4e368c07360846768ea2f59a44da7d4d';
-const isLocal = process.env.NODE_ENV === 'development';
-const redirectUri = isLocal
-  ? 'http://localhost:3000/callback'
-  : 'https://sabrinapalmer.github.io/spotify-playlist-app/callback';
+const redirectUri = window.location.href+'callback'
 
 export { clientId, redirectUri };
