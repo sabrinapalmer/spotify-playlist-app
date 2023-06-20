1420ac8937dc3bdc2e0e04934c6bba391863ba38
sabrinapalmer <sabrinampalmer@gmail.com>
2023-06-20 12:24:52 -0500

    added react spa for gh pages functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './components/Home';
 import Callback from './components/Callback';
+import Login from './components/Login';
+
+
+
 
 function App() {
+  const [accessToken, setAccessToken] = useState('');
+  useEffect(() => {
+    const token = localStorage.getItem('spotifyAccessToken');
+    if (token) {
+      setAccessToken(token);
+    }
+  }, []);
+  const isLocal = process.env.NODE_ENV === 'development';
+
   return (
-    <Router>
+    <Router basename={isLocal?"":"/spotify-playlist-app"}>
       <Routes>
-        <Route path="/spotify-playlist-app" element={<Home />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/spotify-playlist-app" element={accessToken ? <Home accessToken={accessToken} setAccessToken={setAccessToken}/>:<Login />} />
+        <Route path="/" element={accessToken ? <Home accessToken={accessToken} setAccessToken={setAccessToken}/>:<Login />} />
         <Route path="/callback" element={<Callback />} />
       </Routes>
     </Router>
