b6f8ebbb25eb032316326b2db33eca1cb926de4a
sabrinapalmer <sabrinampalmer@gmail.com>
2023-06-20 12:29:53 -0500

    new login screen

---
+++ src/components/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+    clientId,
+    redirectUri
+  } from '../config';
+
+function Login() {
+
+  const authorizeUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(
+    redirectUri
+  )}&response_type=token&scope=playlist-modify-public%20user-top-read`;
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <a href={authorizeUrl}>Login with Spotify</a>
+    </div>
+  );
+}
+
+export default Login;
