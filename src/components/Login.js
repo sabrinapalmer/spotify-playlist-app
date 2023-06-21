f12291e76f3cfe9d4fed017a20378883c3c1a5d0
sabrinapalmer <sabrinampalmer@gmail.com>
2023-06-20 23:01:12 -0500

    prettier

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,7 @@
-import React from 'react';
-import {
-    clientId,
-    redirectUri
-  } from '../config';
+import React from "react";
+import { clientId, redirectUri } from "../config";
 
 function Login() {
-
   const authorizeUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(
     redirectUri
   )}&response_type=token&scope=playlist-modify-public%20user-top-read`;
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const clientId = '4e368c07360846768ea2f59a44da7d4d';
-const redirectUri = window.location.href+'callback'
+const clientId = "4e368c07360846768ea2f59a44da7d4d";
+const redirectUri = window.location.href + "callback";
 
 export { clientId, redirectUri };
